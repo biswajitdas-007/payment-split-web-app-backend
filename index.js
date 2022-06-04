65c232ab8bafd667a22fef31917d617b6354c84e
Biswajit Das <biswajitdas7894@gmail.com>
2022-06-05 04:38:43 +0530

    google auth

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+var cors = require("cors");
+const http = require("http");
+require("dotenv").config();
+require("./auth");
+
+const app = express();
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+
+app.get("/protected", isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`);
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send("Goodbye!");
+});
+
+app.get("/auth/google/failure", (req, res) => {
+  res.send("Failed to authenticate..");
+});
+
+app.listen(PORT, () => console.log("listening on port: 5000"));
