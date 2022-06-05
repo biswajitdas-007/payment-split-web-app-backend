1bc710236c59c3db2b997f021da73ef902ee38d9
Biswajit Das <biswajitdas7894@gmail.com>
2022-06-05 13:40:18 +0530

    cors policy

--- index.js
+++ index.js
@@ -19,14 +19,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
